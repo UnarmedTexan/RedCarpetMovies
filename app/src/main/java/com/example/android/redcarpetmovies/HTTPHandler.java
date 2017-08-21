2f99eac96e22dc35f61923603a46c1f0accc7087
UnarmedTexan <mark@mtmagic.com>
2017-08-21 18:07:32 -0500

    1st submission for Movie app stage 1

--- app/src/main/java/com/example/android/redcarpetmovies/HTTPHandler.java
+++ app/src/main/java/com/example/android/redcarpetmovies/HTTPHandler.java
@@ -8,57 +8,78 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
+import java.nio.charset.Charset;
 
 class HTTPHandler {
     private static final String LOG_TAG = HTTPHandler.class.getSimpleName();
 
     //Empty constructor
-    public HTTPHandler(){
-
+    public HTTPHandler() {
     }
 
     //Method to get the Url connection and the input stream
-    public String getHTTPResponse(String receivedUrl){
-        String response = null;
+    public String getHTTPResponse(String receivedUrl) {
+        String jsonResponse = null;
 
-        try{
+        // If the String receivedUrl is null, then return early.
+        if (receivedUrl == null) {
+            return jsonResponse;
+        }
+
+        HttpURLConnection urlConnection = null;
+        InputStream inputStream = null;
+
+        try {
             URL url = new URL(receivedUrl);
-            HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
-            httpURLConnection.setRequestMethod("GET");
-            InputStream in = new BufferedInputStream(httpURLConnection.getInputStream());
-            response = convertStreamToString(in);
-        }catch (MalformedURLException e){
-            Log.e(LOG_TAG, "MalformedURLException", e.fillInStackTrace());
-        }catch (IOException e){
-            Log.e(LOG_TAG, "IOException", e.fillInStackTrace());
-        }
-        return response;
-    }
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setReadTimeout(10000 /* milliseconds */);
+            urlConnection.setConnectTimeout(15000 /* milliseconds */);
+            urlConnection.setRequestMethod("GET");
+            urlConnection.connect();
 
-    // A method to convert the input stream to a string json to be parsed
-    private String convertStreamToString(InputStream is) {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(is));
-        StringBuilder sb = new StringBuilder();
-        String line;
-
-        try{
-            while ((line=bufferedReader.readLine()) !=null){
-                sb.append(line).append("\n");
+            // If the request was successful (response code 200),
+            // then read the input stream and parse the response.
+            if (urlConnection.getResponseCode() == 200) {
+                inputStream = new BufferedInputStream(urlConnection.getInputStream());
+                jsonResponse = readFromStream(inputStream);
+            } else {
+                Log.e(LOG_TAG, "Error response code: " + urlConnection.getResponseCode());
             }
-        }catch (IOException e){
-            Log.e(LOG_TAG, "IOException at converting to String", e.fillInStackTrace());
-        }finally {
-            try{
-                is.close();
-            }catch (IOException e){
-                Log.e(LOG_TAG, "IO exception while closing input Stream", e.fillInStackTrace());
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "Problem retrieving the news JSON results.", e);
+        } finally {
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
+            if (inputStream != null) {
+                try {
+                    inputStream.close();
+                } catch (IOException ioex) {
+                    Log.e(LOG_TAG, "IO exception while closing input Stream", ioex);
+                }
             }
         }
-        return sb.toString();
+        return jsonResponse;
     }
 
-
+    /**
+     * Convert the {@link InputStream} into a String which contains the
+     * entire JSON response from the MoviesAPI server.
+     */
+    private static String readFromStream(InputStream inputStream) throws IOException {
+        StringBuilder output = new StringBuilder();
+        if (inputStream != null) {
+            InputStreamReader inputStreamReader =
+                    new InputStreamReader(inputStream, Charset.forName("UTF-8"));
+            BufferedReader reader = new BufferedReader(inputStreamReader);
+            String line = reader.readLine();
+            while (line != null) {
+                output.append(line);
+                line = reader.readLine();
+            }
+        }
+        return output.toString();
+    }
 }
 
